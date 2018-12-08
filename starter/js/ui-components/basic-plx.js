e41c4a7640c432f997b49cc2aa33ab35c62bb9c0
Mauricio Flores <contact@maurojflores.com>
2018-12-08 14:57:37 -0600

    Commented parallax componenets; added variations of hero banner component to html demo

--- starter/js/ui-components/basic-plx.js
+++ starter/js/ui-components/basic-plx.js
@@ -2,21 +2,35 @@ window.onload = function() {
     
     class BasicPlxBanner {
         constructor(bannerSelector, backgroundElementSelector, backgroundImageSelector) {
+            // the element that wraps content and parallaxed background
             this.$banner = document.querySelector( bannerSelector );
+            // the element containing the image to use as background for the banner
             this.$bannerBg = this.$banner.querySelector( backgroundElementSelector );
-            this.bannerBgHt = this.$bannerBg.querySelector( backgroundImageSelector ).offsetHeight;
+
+            // the height of the wrapper element
             this.bannerHt = this.$banner.offsetHeight;
+            // the height of the image used as background
+            this.bannerBgHt = this.$bannerBg.querySelector( backgroundImageSelector ).offsetHeight;
+
+            // value to use for `top` css property on image used as background;
+            // if the banner is full-screen height, set to 0; else, center vertically
             this.initialBannerTop = this.$banner.classList.contains('full-screen') 
                 ? 0 
                 : -(this.bannerBgHt - this.bannerHt) / 2;
-        
+            
+            // bind context of all class methods to the created instance object
             this.init = this.init.bind( this );
             this.bindEvents = this.bindEvents.bind( this );
             this.parallax = this.parallax.bind( this );
 
+            // run all initial class functionality
             this.init();
         }
 
+        /**
+         * Set initial `top` position of image being used as background,
+         * Bind event listeners to pertinent DOM elements
+         */
         init() {
             this.$bannerBg.style.top = this.initialBannerTop + 'px';
             this.bindEvents();
@@ -26,12 +40,15 @@ window.onload = function() {
             window.addEventListener( 'scroll', this.parallax.bind(this) );
         }
 
+        /**
+         * Calculate and set new `top` value for banner image based on window scroll position
+         */
         parallax() {
             this.$bannerBg.style.top = this.initialBannerTop - (window.scrollY * 0.15) + 'px';
         }
     }
 
-    // initiate parallax effect
+    // initiate the component
     const plxBanner = new BasicPlxBanner( '.hero-banner', '.plx-background', '.plx-background-img');
 
 };
--- starter/js/ui-components/layered-plx.js
+++ starter/js/ui-components/layered-plx.js
@@ -3,23 +3,36 @@ window.onload = function() {
     class LayeredPlxBanner {
         constructor(elements) {
             this.elements = elements;
+
+            // the element that wraps content and parallaxed background
             this.$banner = document.querySelector( elements.banner );
+            // the element containing the image to use as background for the banner
             this.$bannerBg = this.$banner.querySelector( elements.plxBackground );
 
-            this.bannerBgHt = this.$bannerBg.querySelector( elements.plxBackgroundImg ).offsetHeight;
+            // the height of the wrapper element
             this.bannerHt = this.$banner.offsetHeight;
+            // the height of the image used as background
+            this.bannerBgHt = this.$bannerBg.querySelector( elements.plxBackgroundImg ).offsetHeight;
 
+            // value to use for `top` css property on image used as background;
+            // if the banner is full-screen height, set to 0; else, center vertically
             this.initialBannerTop = this.$banner.classList.contains('full-screen') 
                 ? 0 
                 : -(this.bannerBgHt - this.bannerHt) / 2;
 
+            // bind context of all class methods to the created instance object
             this.init = this.init.bind( this );
             this.bindEvents = this.bindEvents.bind( this );
             this.parallax = this.parallax.bind( this );
 
+            // run all initial class functionality
             this.init();
         }
 
+        /**
+         * Set initial `top` position of image being used as background,
+         * Bind event listeners to pertinent DOM elements
+         */
         init() {
             this.$bannerBg.style.top = this.initialBannerTop + 'px';
             this.bindEvents();
@@ -29,6 +42,9 @@ window.onload = function() {
             window.addEventListener( 'scroll', this.parallax.bind(this) );
         }
 
+        /**
+         * Calculate and set new `top` value for banner image and layers based on window scroll position
+         */
         parallax() {
             this.$bannerBg.style.top = this.initialBannerTop - (window.scrollY * 0.15) + 'px';
             this.elements.layers.forEach( layer => {
@@ -37,8 +53,10 @@ window.onload = function() {
         }
     }
 
-    // get layers
+    // select all layer elements
     const layerElements = [...document.querySelectorAll( '.plx-layer' )];
+
+    // modify layer elements collection to include initial properties
     const layers = layerElements.map( (layerElement, index) => {
         let layerObj = {
             element: layerElement,
@@ -55,7 +73,7 @@ window.onload = function() {
     layers[2].initialTop = 1000;
     layers[2].scrollRatio = 1.5;
 
-    // initiate parallax effect
+    // initiate the component
     const layeredPlxBanner = new LayeredPlxBanner({
         banner: '.hero-banner', 
         layers: layers,
