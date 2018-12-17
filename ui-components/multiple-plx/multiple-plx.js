ac72361e5d31020321898bff282759c944f25282
Mauricio Flores <contact@maurojflores.com>
2018-12-17 13:51:57 -0600

    Changed multiple-plx to calculate heightAboveBanner using .offsetTop

--- ui-components/multiple-plx/multiple-plx.js
+++ ui-components/multiple-plx/multiple-plx.js
@@ -43,52 +43,30 @@ window.addEventListener('load', () => {
          */
         init() {
             this.$bannerBg.style.top = this.initialBannerTop + 'px';
-            this.getHeightAboveBanner();
             this.bindEvents();
         }
 
         bindEvents() {
             window.addEventListener( 'scroll', this.parallax );
         }
-
-        /**
-         * Get the total vertical space above the banner element by adding height of
-         * sibling elements before it. This is to determine at what scroll point to start
-         * the parallax effect
-         */
-        getHeightAboveBanner() {
-            // all sibling elements, including current element
-            const $bannerSiblings = [...this.$banner.parentElement.children];
-            // index of parallax element in siblings array
-            const bannerIndex = $bannerSiblings.indexOf( this.$banner );
-
-            this.heightAboveBanner = 0;
-            // add the height of every sibling element before the parallax element
-            for ( let i = bannerIndex - 2; i >= 0; i-- ) {
-                this.heightAboveBanner += $bannerSiblings[i].offsetHeight;
-            }
-        }
         
         /**
          * Calculate and set new `top` value for banner image based on window scroll position
          */
         parallax() {
-            // if the user has scrolled the parallax element into view
-            if ( (this.heightAboveBanner + (this.bannerHt*2)) >= window.scrollY 
-                    && window.scrollY >= this.heightAboveBanner ) {
-                    
-                    // amount of pixels to shift background, based on element's vertical position on the page
-                    const plxScroll = (window.scrollY - this.heightAboveBanner) * this.scrollRatio;
-                    // set new `top` value on background
-                    this.$bannerBg.style.top = this.initialBannerTop - plxScroll + 'px';
+            let isElementVisible = this.$banner.offsetTop - window.innerHeight <= window.scrollY
+                && window.scrollY <= this.$banner.offsetHeight + (this.bannerHt*2);
+                
+            if ( isElementVisible ) {
+                // amount of pixels to shift background, based on element's vertical position on the page
+                const plxScroll = window.scrollY * this.scrollRatio;
+                // set new `top` value on background
+                this.$bannerBg.style.top = this.initialBannerTop - plxScroll + 'px';
             }
         }
     }
 
     // initiate the component
-    // const plxBanner = new MultiplePlxBanner( '#banner-1', '.plx-background', '.plx-background-img');
-    // const plxBanner2 = new MultiplePlxBanner( '#banner-3', '.plx-background', '.plx-background-img', 0.25);
-    // bannerSelector, backgroundElementSelector, backgroundImageSelector, scrollRatio = 0.15
     const plxBanner = new MultiplePlxBanner({
         bannerSelector: '#banner-1', 
         backgroundSelector: '.plx-background', 
